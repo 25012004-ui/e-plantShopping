314989ac1e2095a9eb20bd1d6eac2a4def375655
PRIYANSHU PRAJAPATI <22bcs15046@cuchd.in>
2026-04-23 16:10:52 +0530

    Create Aboutus component for company overview
    Added Aboutus component to provide company information.

---
+++ AboutUs.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Aboutus = () => {
+  return (
+    <div>
+      <h1>About Future Mobility Solutions</h1>
+      <p>
+        Future Mobility Solutions is a company focused on developing innovative
+        electric vehicle technologies, including battery systems, smart charging,
+        and sustainable mobility solutions.
+      </p>
+      <p>
+        Our mission is to accelerate the transition to clean and efficient
+        transportation by leveraging cutting-edge engineering and software.
+      </p>
+      <p>
+        We specialize in EV system design, energy management, and intelligent
+        mobility platforms that contribute to a greener future.
+      </p>
+    </div>
+  );
+};
+
+export default Aboutus;
